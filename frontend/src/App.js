f03d9fe11fa8969e200aab70fa1ff87f545c1bc6
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-02-17 15:34:21 +0100

    frontend initial

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
